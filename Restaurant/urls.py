3ee665dda032e6a5c4c73ba385d5b79e50c12e3d
Muhammad Radwan <muhammed.a.radwan@gmail.com>
2025-04-26 21:59:51 +0300

    feat: menu apis

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -8,5 +8,7 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('menu', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, MenuSerializer
+from .models import Menu
+from rest_framework import generics
 
 def index(request):
     return render(request, 'index.html', {})
@@ -10,3 +12,15 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
